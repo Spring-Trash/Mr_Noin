7ac175f1d5a4186e6b3233816733abfec388c238
cbnu2017038040@gmail.com <wx47631!@#>
2023-11-06 19:14:23 +0900

    [ADD] Add methods to going board, notice

--- src/main/java/com/spring/mrnoin/controller/IndexController.java
+++ src/main/java/com/spring/mrnoin/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -62,5 +63,10 @@ public class IndexController {
 
     @RequestMapping("/noview")
     public String noview(){return "test"; }
+
+    @GetMapping("/board")
+    public String board(){
+        return "";
+    }
 
 }
